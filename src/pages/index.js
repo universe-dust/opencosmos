cc8c09a6a722307ea581d2267d32ea5dcb2e0f32
snow <chengcheng.duan@outlook.com>
2022-05-01 22:17:37 +0800

    po

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,9 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            查看 - 5min ⏱️
           </Link>
+
         </div>
       </div>
     </header>
@@ -33,7 +34,7 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-//        <HomepageFeatures />
+        <HomepageFeatures />
       </main>
     </Layout>
   );
